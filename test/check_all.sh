86224f55ae7343c68233ecb455a59956895108c9
vladimirtepes <1905025879@qq.com>
2018-04-18 11:55:08 +0800

    将check_asc.sh和check_trc.sh合并为check_all.sh

---
+++ test/check_all.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+tempfile=temp.txt
+
+while [ $# -ge 1 ]
+do
+	dir=$(dirname $1)
+	suffix=$1
+	suffix=${suffix##*.}
+	suffix=$(echo $suffix | tr [A-Z] [a-z])
+	filename=${dir}'/'$(basename -s .trc $1).txt
+	echo '' > $filename
+
+	echo $1' -> '$filename
+
+	if [ $suffix == "trc" ];then
+		grep -vE '^;' $1 | awk '{printf $2"\t";printf $4"\t";for(i=7;i<=NF;++i){printf $i" "};print ""}' > $tempfile
+	elif [ $suffix == "asc" ];then
+		grep -E '^ ' $1 | awk '{if($2 == "1" && $3 != "Statistic:")print}' | awk '{printf $1"\t";printf $3"\t";for(i=7;i<=14;++i){printf $i" "};print ""}' > $tempfile
+	else
+		return 1
+	fi
+
+
+	echo 'engine info:' >> $filename
+	cat $tempfile | ./canm engine.cfg >> $filename 
+	echo '' >> $filename
+
+	echo '0x0E3:' >> $filename
+	cat $tempfile | ./canm 0x0E3.cfg >> $filename
+	echo '' >> $filename
+
+#	echo '0x2B1:' >> $filename
+#	cat $tempfile | ./canm 0x2B1.cfg >> $filename
+#	echo '' >> $filename
+#
+#	echo '0x2C8:' >> $filename
+#	cat $tempfile | ./canm 0x2C8.cfg >> $filename
+#	echo '' >> $filename
+#
+#	echo '0x2AD:' >> $filename
+#	cat $tempfile | ./canm 0x2AD.cfg >> $filename
+#	echo '' >> $filename
+
+	echo 'remote info:' >> $filename
+	cat $tempfile | ./canm remote.cfg >> $filename
+	echo '' >> $filename
+
+	echo '0x297:' >> $filename
+	cat $tempfile | ./canm 0x297.cfg >> $filename
+	echo '' >> $filename
+
+	shift
+done
